85f82ca53aef60164e92e478489eba39d83c57e1
tomohiro_0225 <express22212270@gmail.com>
2024-06-29 15:53:47 +0900

    g-planning開発データプッシュ

---
+++ gp-theme2/js/index.js
@@ -0,0 +1,15 @@
+// スライダー
+$(function () {
+  $('.top-work__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 5000,
+    cssEase: "linear",
+  });
+});
